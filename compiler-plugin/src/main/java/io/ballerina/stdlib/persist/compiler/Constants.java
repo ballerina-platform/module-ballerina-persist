2724a27a80127b606cc95bc393bf85354fb1d95f
Kalaiyarasiganeshalingam <ashakalai31@gmail.com>
2022-12-21 12:54:31 +0530

    Add missing file in the ignore list
    Add whitespace
    
    Add private constructors
    
    Add new lines

--- compiler-plugin/src/main/java/io/ballerina/stdlib/persist/compiler/Constants.java
+++ compiler-plugin/src/main/java/io/ballerina/stdlib/persist/compiler/Constants.java
@@ -30,6 +30,8 @@ import static io.ballerina.compiler.syntax.tree.AbstractNodeFactory.createEmptyM
  */
 public class Constants {
 
+    private Constants() {}
+
     public static final String PERSIST_MODULE = "persist";
     public static final String TIME_MODULE = "time";
 
@@ -84,6 +86,9 @@ public class Constants {
      * Constants related to Ballerina types.
      */
     public static final class BallerinaTypes {
+
+        private BallerinaTypes() {}
+
         public static final String INT = "int";
         public static final String STRING = "string";
         public static final String BOOLEAN = "boolean";
@@ -95,6 +100,9 @@ public class Constants {
      * Constants related to Ballerina time type.
      */
     public static final class BallerinaTimeTypes {
+
+        private BallerinaTimeTypes() {}
+
         public static final String DATE = "Date";
         public static final String TIME_OF_DAY = "TimeOfDay";
         public static final String UTC = "Utc";
@@ -105,6 +113,9 @@ public class Constants {
      * SQL keywords used to construct the query.
      */
     public static final class SQLKeyWords {
+
+        private SQLKeyWords() {}
+
         public static final String WHERE = "WHERE";
         public static final String LIMIT = "LIMIT";
         public static final String ORDERBY = "ORDER BY";
@@ -119,6 +130,9 @@ public class Constants {
      * Constant nodes used in code modification.
      */
     public static final class TokenNodes {
+
+        private TokenNodes() {}
+
         public static final Token INTERPOLATION_START_TOKEN = NodeFactory.createLiteralValueToken(
                 SyntaxKind.INTERPOLATION_START_TOKEN, "${", createEmptyMinutiaeList(), createEmptyMinutiaeList());
         public static final Token INTERPOLATION_END_TOKEN = NodeFactory.createLiteralValueToken(
