f9f9cf68fd86c2beec4050ec91d3ed0b04c3d738
kaneeldias <kaneeldias@hotmail.com>
2023-01-30 14:06:14 +0530

    Revert changes to initialization SQL script

--- ballerina/tests/resources/sql-scripts/init.sql
+++ ballerina/tests/resources/sql-scripts/init.sql
@@ -1,5 +1,26 @@
 CREATE Database test;
 
+CREATE TABLE test.Building (
+    buildingCode VARCHAR(36) PRIMARY KEY,
+    city VARCHAR(50),
+    state VARCHAR(50),
+    country VARCHAR(50),
+    postalCode VARCHAR(50),
+    type VARCHAR(50)
+);
+
+CREATE TABLE test.Workspace (
+    workspaceId VARCHAR(36) PRIMARY KEY,
+    workspaceType VARCHAR(10),
+    buildingBuildingCode VARCHAR(36),
+    FOREIGN KEY (buildingBuildingCode) REFERENCES test.Building(buildingCode)
+);
+
+CREATE TABLE test.Department (
+    deptNo VARCHAR(36) PRIMARY KEY,
+    deptName VARCHAR(30)
+);
+
 CREATE TABLE test.Employee (
     empNo VARCHAR(36) PRIMARY KEY,
     firstName VARCHAR(30),
@@ -13,32 +34,10 @@ CREATE TABLE test.Employee (
     FOREIGN KEY (workspaceWorkspaceId) REFERENCES test.Workspace(workspaceId)
 );
 
-CREATE TABLE test.Workspace (
-    workspaceId VARCHAR(36) PRIMARY KEY,
-    workspaceType VARCHAR(10),
-    buildingBuildingCode VARCHAR(36),
-    FOREIGN KEY (buildingBuildingCode) REFERENCES test.Building(buildingCode)
-);
-
-CREATE TABLE test.Building (
-    buildingCode VARCHAR(36) PRIMARY KEY,
-    city VARCHAR(50),
-    state VARCHAR(50),
-    country VARCHAR(50),
-    postalCode VARCHAR(50),
-    type VARCHAR(50)
-);
-
-CREATE TABLE test.Department (
-    deptNo VARCHAR(36) PRIMARY KEY,
-    deptName VARCHAR(30)
-);
-
 CREATE TABLE test.OrderItem (
     orderId VARCHAR(36),
     itemId VARCHAR(30),
     quantity INTEGER,
     notes VARCHAR(255),
-    PRIMARY KEY(orderId, itemId)  
+    PRIMARY KEY(orderId, itemId)
 );
-
