ba66f36cc2cffe63005baac170412ffa2b913d96
Bhashinee <Bhashinee@users.noreply.github.com>
2024-04-02 10:26:05 +0530

    Address review suggestions
    Co-authored-by: Danesh Kuruppu <daneshk@users.noreply.github.com>

--- compiler-plugin/src/main/java/io/ballerina/stdlib/persist/compiler/utils/Utils.java
+++ compiler-plugin/src/main/java/io/ballerina/stdlib/persist/compiler/utils/Utils.java
@@ -221,6 +221,7 @@ public final class Utils {
                     }
                 }
             }
+            // Skip data store-specific validations if the `tool.persist` configuration does not exist in the `toml` files, instead of returning an error.
             return null;
         } catch (IOException e) {
             throw new BalException("error while reading persist configurations. " + e.getMessage());
